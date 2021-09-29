5ce84c203824394879542e1ed98abc67d5f9c066
EdwinTomy <57510528+EdwinTomy@users.noreply.github.com>
2021-09-28 19:45:31 -0600

    final changes to ui selection

--- src/uimain.c
+++ src/uimain.c
@@ -1,13 +1,40 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "tokenizer.h"
+#include "history.h"
 
 int main()
 {
-  char userIn[1000];
+  printf("Welcome!:\n)");
+  List* list = init_history();
   while(1){
-    puts("$");
-    fgets(userIn, 100, stdin);
-    printf("%s\n",userIn);
+    printf("Select one of the following inputs, please:\n);
+    printf("1. Print full history (type <h>)\n");
+    printf("2. Get specific node i from history (type <!i>, where i is a n)\n");
+    printf("3. Quit program (type <q>)\n");
+    printf("4. Tokenize! (type anything else)\n");
+          
+    char *input = malloc(sizeof(char*));
+    fgets(input, 100, stdin);
+
+    if(input[0] == 'h'){
+      print_history(list);
+
+    } else if(input[0] == '!'){
+      int x = input[1] - '0';
+      printf("Node at id[%d]:  %s\n", x, get_history(list, x));
+
+    } else if(input[0] == 'q'){
+      puts("Goobdbye! :(");
+      break;
+      
+    } else{
+      puts("Tokenized String:");
+      char ** tokens = tokenize(input);
+      add_history(list, userIn);
+      print_tokens(tokens);
+      free_tokens(tokens);
+    }
   }
   return 0;
 }
