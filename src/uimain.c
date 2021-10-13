ec4caa517172fdda6d6589d4ae6d72cd91b38e46
EdwinTomy <57510528+EdwinTomy@users.noreply.github.com>
2021-10-13 10:59:12 -0600

    Update uimain.c

--- src/uimain.c
+++ src/uimain.c
@@ -1,35 +1,30 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "tokenizer.h"
 #include "history.h"
 
 int main()
 {
-  char user_input[1000];
-  List *list = init_history();
-  int bool = 1; 
-  
-  puts("Welcome to Tokenizer!\n");
-  
-  while (bool) {
-    puts("Input a string to tokenize it,  !<n> to obtain nth node, !h for history, !q to quit:\n");
-    printf("->");
-    fgets(user_input,1000, stdin);
+  char user_input[100];
+  struct s_List *list = init_history();
+  int cont = 1; 
+  puts("Welcome!\n");
+  while (cont) {
+    puts("Input a string to tokenize it of !<num> for a special character, !h for full history, !q for to quit:\n");
+    printf("> ");
+    fgets(user_input,100, stdin);
     
     if (user_input[0] == '!' && user_input[1] == 'h') {
       print_history(list);
     }
-    
     else if (user_input[0] == '!' && user_input[1] == 'q') {
-      bool = 0;
-      printf("Bye! >:(\n");
+      cont = 0;
+      printf("Thank you, Goodbye :)\n");
     }
-    
     else if (user_input[0] == '!') {
       char *history_val = get_history(list, user_input[1] - '0');
-      printf("Node[%d] is:%s\n", user_input[1] - '0', history_val);
+      printf("H[%d] %s\n", user_input[1] - '0', history_val);
     }
-    
     else {
       char **tokens = tokenize(user_input);
       printf("%s\n", tokens[0]);
@@ -37,7 +32,6 @@ int main()
       add_history(list, user_input);
     }
   }
-  
   free_history(list);
   return 0;
 }
