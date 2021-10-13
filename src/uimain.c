e2afa84b3fefc9f4ee2cb2631eabda1f26d46c0a
EdwinTomy <57510528+EdwinTomy@users.noreply.github.com>
2021-10-13 08:18:10 -0600

    Update uimain.c

--- src/uimain.c
+++ src/uimain.c
@@ -11,6 +11,7 @@ int main()
   printf(word_start("hol   rotrf"));
   printf(word_terminator("   rotrf"));
   printf(word_terminator("rotrf.  rfjh"));
+  printf("%d\n",5);
   printf("%d\n",count_words("  cerc erc crc cr c.  "));
   
   printf("Welcome!:\n)");
