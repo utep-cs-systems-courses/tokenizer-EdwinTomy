b137bbc0405b7075a67b6bb53ae5198097500b07
EdwinTomy <57510528+EdwinTomy@users.noreply.github.com>
2021-10-14 14:27:34 -0600

    Update tokenizer.c

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -16,24 +16,34 @@ int non_space_char(char c)
 
 char *word_start(char *str)
 {
+  printf("---------------\n");
+  printf("Start ended with:%s\n", str);
   while(space_char(*str++));
 
   if(*str == '\0') {
     char *p = NULL;
+    printf("Start ended with:%s\n", str);
+    printf("---------------\n");
     return p;
   }
   
   str--;
+  printf("Start ended with:%s\n", str);
+  printf("---------------\n");
   return str;
 }
 
 char *word_terminator(char *str)
 {
+  printf("--------------------\n");
+  printf("Terminator:%s\n", str);
   str = word_start(str);
  
   if(*str == '\0') {
     printf("is null\n");
     char *p = NULL;
+    printf("Terminator ended with:%s\n", p);
+    printf("--------------------\n");
     return p;
   }
   
@@ -41,11 +51,15 @@ char *word_terminator(char *str)
     str++;
   }
 
+  printf("Terminator ended with:%s\n", str);
+  printf("--------------------\n");
   return str;
 }
 
 int count_words(char *str)
 {
+  printf("~~~~~~~~~~~~~~~~~~~~\n");
+  printf("Count with:%s\n", str);
   int cnt = 0;
   str = word_start(str);
    
@@ -55,12 +69,16 @@ int count_words(char *str)
     str = word_start(str);
   }
    
+  printf("Count with:%d\n", cnt);
+  printf("~~~~~~~~~~~~~~~~~~~~\n");
   return cnt;
 }
 
 
 char *copy_str(char *inStr, short len)
 {
+  printf("~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+  printf("Copy with:%s;and len:%d\n", str, len);
   char *copy = (char *) malloc((len + 1) * sizeof(char));
    
   int i;
@@ -69,11 +87,15 @@ char *copy_str(char *inStr, short len)
   }
    
   copy[i] = '\0';
+  printf("End Copy with:%s\n", copy);
+  printf("~~~~~~~~~~~~~~~~~~~~~~~~~\n");
   return copy;
 }
 
 char **tokenize(char* str)
 {
+  printf("#########################\n");
+  printf("Tokenize:%s\n", str);
   int cnt = count_words(str);
   char **tokens = (char **) malloc((cnt + 1) * sizeof(char*));
   printf("cnt is %d\n", cnt);
@@ -81,26 +103,30 @@ char **tokenize(char* str)
   for(int i = 0; i < cnt; i++) {
     str = word_start(str);
     tokens[i] = copy_str(str, word_terminator(str) - str);
+    printf("Print token %s\n", token[i]);
     str = word_terminator(str);
   }
   
   tokens[cnt] = 0; 
+  printf("End of tokenize\n");
+  printf("#########################\n");
   return tokens;
 }
 
 void print_tokens(char **tokens)
 {
-  printf("Tokens:\n");
+  printf("##############################\n");
+  printf("Printing tokens\n");
   int cnt = 0;
    
-  printf("Print the 3rd:%s\n",tokens[3]);
   while (tokens[cnt] != NULL) {
     printf("Token[%d]:%s\n", cnt+1, tokens[cnt]);
     cnt++;
     printf("Print cnt:%d\n",cnt);
   }
-  printf("end\n");
   
+  printf("Printed tokens\n");
+  printf("##############################\n");
 }
 
 void free_tokens(char **tokens)
