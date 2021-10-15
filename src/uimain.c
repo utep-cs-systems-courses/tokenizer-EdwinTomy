bac60d06625fd7b625f4896f31204f3d0f987300
EdwinTomy <57510528+EdwinTomy@users.noreply.github.com>
2021-10-14 18:59:19 -0600

    Update uimain.c

--- src/uimain.c
+++ src/uimain.c
@@ -5,34 +5,40 @@
 
 int main()
 {
-  word_start(NULL);
-  word_terminator(NULL);
   char user_input[100];
   struct s_List *list = init_history();
-  int cont = 1; 
-  puts("Welcome!\n");
-  while (cont) {
-    puts("Input a string to tokenize it of !<num> for a special character, !h for full history, !q for to quit:\n");
-    printf("> ");
+  int if_continue = 1; 
+  puts("Welcome! :) \n");
+  
+  while (if_continue) {
+    puts("Input !<num> to access string at index num,\n");
+    puts("or input !h for full history of strings,\n");
+    puts("or input !q to quit tokenizer,\n");
+    puts("or input any other string to tokenize it:\n");
+    printf("-->");
     fgets(user_input,100, stdin);
     
     if (user_input[0] == '!' && user_input[1] == 'h') {
       print_history(list);
     }
+    
     else if (user_input[0] == '!' && user_input[1] == 'q') {
-      cont = 0;
-      printf("Thank you, Goodbye :)\n");
+      if_continue = 0;
+      printf("Goodbye! >:( \n");
     }
+    
     else if (user_input[0] == '!') {
-      char *history_val = get_history(list, user_input[1] - '0');
-      printf("H[%d] %s\n", user_input[1] - '0', history_val);
+      char *string_at_idx = get_history(list, user_input[1] - '0');
+      printf("String at #id[%d]:%s\n", user_input[1] - '0', string_at_idx);
     }
+    
     else {
       char **tokens = tokenize(user_input);
       print_tokens(tokens);
       add_history(list, user_input);
     }
   }
+  
   free_history(list);
   return 0;
 }
